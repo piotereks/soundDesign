9d0234a70a448a88a4d41a1b4aeeb80b7967bd10
Piotr Sakowski <piotereks@gmail.com>
2022-05-08 21:52:18 +0200

    Isobar research

--- main.py
+++ main.py
@@ -6,11 +6,16 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
+    # FIXME: QQRYQ
+
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# FIXME asdfasdfs fsdfs f
+#  blajdfsdfad sdf
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    # TODO: asdfsdfafed
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
