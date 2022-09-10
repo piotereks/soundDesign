5f25b897d72e502b96de7e88ffbd5628730e8ee4
Piotr Sakowski <piotereks@gmail.com>
2022-09-10 10:40:08 +0000

    pyinput model checked (to be converted to class)

--- tracker/pynputMod.py
+++ tracker/pynputMod.py
@@ -5,32 +5,42 @@ import time
 from pynput import keyboard
 
 
+global prev_key
+prev_key = None
+
 
 def on_press(key):
-    try:
-        print('alphanumeric key {0} pressed'.format(
-            key.char))
+    global prev_key
+    if not prev_key:
+        prev_key = key
+        try:
+            print('alphanumeric key {0} pressed'.format(
+                key.char))
+            note = play_keys.index(key.char)+60 if key.char in play_keys else None
+            print(f'{note=}')
 
-    except AttributeError:
-        print('special key {0} pressed'.format(
-            key))
-    print(f'key again {key=}')
+        except AttributeError:
+            print('special key {0} pressed'.format(
+                key))
+
+
 
 def on_release(key):
+    global prev_key
     print('{0} released'.format(
         key))
+    prev_key = None
     if key == keyboard.Key.esc:
         # Stop listener
         return False
 
-keys = "q2w3er5t6y7ui9o0p[=]"
+play_keys = "q2w3er5t6y7ui9o0p[=]"
 # Collect events until released
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
 
-q2w3er5t6y7ui9o0p[
 
 
 # ...or, in a non-blocking fashion:
