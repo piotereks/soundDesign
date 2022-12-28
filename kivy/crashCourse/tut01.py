8eb42873ff6d595b16030de6c4ba3c71922649d2
piotereks <69604748+piotereks@users.noreply.github.com>
2022-12-28 14:15:23 +0000

    tutorial Kivy crash course WIP

---
+++ kivy/crashCourse/tut01.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TutorialApp(App):
+    def build(self):
+        return Button(text='Hello!',
+                      background_color=(0, 0, 1, 1),  # List of
+                                                      # rgba components
+                      font_size=150)
+
+if __name__ == "__main__":
+    TutorialApp().run()
