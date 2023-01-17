3ba63100bc6a0c33d9fec56c3a74f59697646d90
piotereks <69604748+piotereks@users.noreply.github.com>
2023-01-17 22:32:56 +0000

    #38 working remove button

--- kivy/sepWindow/ScalesCheck_main.py
+++ kivy/sepWindow/ScalesCheck_main.py
@@ -6,7 +6,7 @@ SlideTransition, CardTransition, SwapTransition,
 FadeTransition, WipeTransition, FallOutTransition, RiseInTransition)
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.behaviors import ToggleButtonBehavior
-from kivy.properties import StringProperty, ListProperty
+from kivy.properties import StringProperty, ListProperty, ObjectProperty
 
 class MainScreen(Screen):
 
@@ -25,6 +25,8 @@ class RadioButton(ToggleButtonBehavior, BoxLayout):
 
 class ScalesChkApp(App):
     selected_scale_button = StringProperty()
+    btn = ObjectProperty()
+    button_matrix = ListProperty()
     # ScalesSelectScreen.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 1'))
     # ScalesSelectScreen.ids.button_grid.add_widget(RadioButton(text='World 3'))
 
@@ -38,12 +40,13 @@ class ScalesChkApp(App):
     
     def rem_buttons(self):
         for button in self.button_matrix:
-            self.root.remove_widget(button) 
+            # self.root.ids.scales_opt.remove_widget(button) 
+            self.root.ids.scales_opt.ids.button_grid.remove_widget(button)
         self.but_id_offset+=100
     
-    def on_start(self):
+    def populate_button(self):
         self.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 2'))
-        self.button_matrix=[]
+        # self.button_matrix=[]
         for button_id in range(self.but_id_offset,self.but_id_offset+10):
             btn = RadioButton(text=f'auto_{button_id}')
             self.button_matrix.append(btn)
