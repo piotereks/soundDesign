91a9bb22e4ce3a57bf7c955b3384ed1c310f45d9
piotereks <69604748+piotereks@users.noreply.github.com>
2023-01-21 00:39:52 +0000

    #38 swip gesture WIP

--- kivy/sepWindow/ScalesCheck_main.py
+++ kivy/sepWindow/ScalesCheck_main.py
@@ -10,56 +10,57 @@ from kivy.properties import (StringProperty, ListProperty, ObjectProperty, Numer
 
 class MainScreen(Screen):
 
+
+    # def on_touch_down(self,touch): 
+    #     print(f"--down----------------{self}, {touch}") 
+    #     return super(MainScreen, self).on_touch_down(touch)
+
+    # def on_touch_up(self,touch): 
+    #     print(f"--up----------------{self}, {touch}")
+    #     return super(MainScreen, self).on_touch_up(touch)
     pass
 
 class ScalesSelectScreen(Screen):
-    # def on_start(self):
-    # ScalesSelectScreen.ids.button_grid.add_widget(RadioButton(text='World 2'))
-
-    pass
-
-class RadioButton(ToggleButtonBehavior, BoxLayout):
-    text = StringProperty('')
-    pass
 
 
-class ScalesChkApp(App):
-    selected_scale_button = StringProperty()
     btn = ObjectProperty()
     button_matrix = ListProperty()
-    grid_rows=NumericProperty()
-    grid_cols=NumericProperty()
-    grid_len=NumericProperty()
-    # ScalesSelectScreen.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 1'))
-    # ScalesSelectScreen.ids.button_grid.add_widget(RadioButton(text='World 3'))
+    grid_rows=NumericProperty(13)
+    grid_cols=NumericProperty(5)
+    grid_len=NumericProperty(13*5)
+    pos_x = NumericProperty()
 
-    # ScreenManager.transition=RiseInTransition()
-    # def populate_button(self):
     but_id_offset = 0
     button_names = [ 'button_'+ str(i+1).rjust(3,'0')[-3:] for i in range(500)]
-    nbr_of_scales = len(button_names)
+    nbr_of_scales = len(button_names)   
 
-    def __init__(self, rows, cols):
-        super(ScalesChkApp, self).__init__()
-        # pass
-        self.grid_rows=rows
-        self.grid_cols=cols
-        self.grid_len=rows*cols
-        
-    # def build(self):
-    #     return ScreenManager()
-
-
-    def on_selected_scale_button(self, instance, value):
-        print(instance, value)
-        print(self.selected_scale_button)
     
+    def populate_button(self):
+        button_matix_len=self.grid_cols*self.grid_rows
+        # self.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 2'))
+        # self.button_matrix=[]
+        # for button_id in range(self.but_id_offset,self.but_id_offset+10):
+        # for button_id in self.button_names:
+        for button_id in self.button_names[self.but_id_offset:self.but_id_offset+self.grid_len]:
+            btn = RadioButton(text=f'{button_id}')
+            self.button_matrix.append(btn)
+            # self.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text=f'auto_{button_id}'))
+            # self.root.ids.scales_opt.ids.button_grid.add_widget(btn)
+            self.ids.button_grid.add_widget(btn)
+            
+        print('----------------')
+    pass
+
+
     def rem_buttons(self):
         for button in self.button_matrix:
             # self.root.ids.scales_opt.remove_widget(button) 
-            self.root.ids.scales_opt.ids.button_grid.remove_widget(button)
+            # self.root.ids.scales_opt.ids.button_grid.remove_widget(button)
+            self.ids.button_grid.remove_widget(button)
         # self.but_id_offset+=self.grid_len
     
+
+
     def scale_page(self, direction):
         if direction in ('RL','prev'):
             self.rem_buttons()
@@ -75,27 +76,66 @@ class ScalesChkApp(App):
             return    
         self.populate_button()
      
-    def on_touch_move(self,touch):    
-        if touch.dx > 0:
+    def on_touch_down(self,touch):
+        # print(f"down , {touch.__dict__=}, {touch.px=}, {touch.py=}, {touch.pos=}")
+        self.pos_x=touch.px
+        # return super(ScalesSelectScreen, self).on_touch_down(touch)
+        # touch.grab(self)
+        
+    def on_touch_up(self,touch):
+        # print(f"up, {touch.__dict__=}, {touch.px=}, {touch.py=}, {touch.pos=}")
+        if self.pos_x-touch.px > 50:
+            print('next')
             self.scale_page('next')
-        elif touch.dx < 0:
+        elif self.pos_x-touch.px <-50:
+            print('prev')
             self.scale_page('prev')
+            # return super(ScalesSelectScreen, self).on_touch_down(touch)
+        # touch.grab(self)
 
 
-    def populate_button(self):
-        button_matix_len=self.grid_cols*self.grid_rows
-        # self.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 2'))
-        # self.button_matrix=[]
-        # for button_id in range(self.but_id_offset,self.but_id_offset+10):
-        # for button_id in self.button_names:
-        for button_id in self.button_names[self.but_id_offset:self.but_id_offset+self.grid_len]:
-            btn = RadioButton(text=f'{button_id}')
-            self.button_matrix.append(btn)
-            # self.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text=f'auto_{button_id}'))
-            self.root.ids.scales_opt.ids.button_grid.add_widget(btn)
-            
-        print('----------------')
+    # def on_touch_move(self,touch): 
+    #     # print(f"----------------------{self}, {touch} {touch.dx}") 
+    #     print(f"{touch.dx}") 
+    #     # touch.ungrab(self)
+    #     if touch.dx < -50:
+    #         print('next')
+    #         self.scale_page('next')
+    #     elif touch.dx > 50:
+    #         print('prev')
+    #         self.scale_page('prev')
+
+
+        
+        
+class RadioButton(ToggleButtonBehavior, BoxLayout):
+    text = StringProperty('')
     pass
+
+
+class ScalesChkApp(App):
+ 
+    selected_scale_button = StringProperty() 
+    
+    # ScalesSelectScreen.root.ids.scales_opt.ids.button_grid.add_widget(RadioButton(text='World 1'))
+    # ScalesSelectScreen.ids.button_grid.add_widget(RadioButton(text='World 3'))
+
+
+    def __init__(self, rows, cols):
+        super(ScalesChkApp, self).__init__()
+        self.grid_rows=rows
+        self.grid_cols=cols
+        self.grid_len=rows*cols
+
+
+    def on_selected_scale_button(self, instance, value):
+        print(instance, value)
+        print(self.selected_scale_button)    
+
+
+
+
+
 
 #  self.manager.ids.another.ids.box.add_widget(Label(text="Button 1 pressed"))
 
