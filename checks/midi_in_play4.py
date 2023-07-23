fcc7c03e3c36f580a09e29eab432d25a268c7e9d
Piotr Sakowski <piotereks@gmail.com>
2023-07-23 14:03:25 +0200

    #113 - WIP

--- checks/midi_in_play4.py
+++ checks/midi_in_play4.py
@@ -7,7 +7,7 @@ from isobar import *
 from tracker.app.isobar_fixes import *
 
 import tracker.app.mido_fixes
-
+from functools import partial
 
 x = 1
 # file = os.path.join('example_midi', 'Variable_tempo_one_note_mod.mid')
@@ -28,13 +28,18 @@ for pattern in patterns:
     action = pattern.pop(EVENT_ACTION, None)
     # action_fun  = [lambda x=x: f(timeline, x) for f, x in action]
     # action_funx = iso.PSequence(action_fun)
-    action_fun = iso.PSequence([lambda msg=msg: f(timeline, msg) for f, msg in action])
+    # xxx = [lambda msg=msg: f(timeline, msg) for f, msg in action]
+    xxx = [partial(f, msg) for f, msg in action]
+    # yyy = list(xxx)
+    action_fun = iso.PSequence(xxx, repeats=1)
+    # action_fun = iso.PSequence([lambda msg=msg: f(timeline, msg) for f, msg in action], repeats=1)
     # xxx = action_fun[11](None)
+    flag = True
     if action:
         # timeline.schedule({EVENT_ACTION: action_fun})
-        timeline.schedule({EVENT_ACTION: action_fun})
-    timeline.schedule(pattern)
-timeline.background()
-# timeline.run()
+        timeline.schedule({EVENT_ACTION: action_fun}, remove_when_done=flag)
+    timeline.schedule(pattern, remove_when_done=flag)
+# timeline.background()
+timeline.run()
 
 x = 1
