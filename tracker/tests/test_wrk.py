35dd1aa50e0e6b478a74359f1fd9c96dd57f20b8
Piotr Sakowski <piotereks@gmail.com>
2023-12-17 20:35:55 +0100

    #134 - fix of wrongly placed notes (separate fix)

--- tracker/tests/test_wrk.py
+++ tracker/tests/test_wrk.py
@@ -603,10 +603,10 @@ def test_pattern_len(dummy_timeline, dummy_timeline2):
                                                      lambda track_idx: set_tempo(300), lambda track_idx: set_tempo(200)], repeats=1)
 
     }
+    dummy_tim.schedule(pgm, sel_track_idx=0)
+    dummy_tim.schedule(pgm2, sel_track_idx=1)
+    dummy_tim.schedule(events_action, sel_track_idx=0)
 
-    # dummy_tim.schedule(pgm, sel_track_idx=0)
-    # dummy_tim.schedule(pgm2, sel_track_idx=1)
-    # dummy_tim.schedule(events_action, sel_track_idx=0)
     dummy_tim.schedule(events, sel_track_idx=0)
     dummy_tim.schedule(events2, sel_track_idx=1)
 
